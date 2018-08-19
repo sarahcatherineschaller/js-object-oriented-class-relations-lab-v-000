bf8847aaaff6be1fcf1577ff70ee6bc52ca5899e
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-19 19:37:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ class Passenger {
       return trip.passengerId === this.id;
     });
   }
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver();
+    });
+  }
 }
 
 class Trip {
