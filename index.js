a4b0d2753d2384ccd6797e6e2e66bf1c3303382c
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-19 19:35:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,9 +24,19 @@ class Driver {
 
 class Passenger {
   constructor(name) {
-    this.id = ++passengerId 
-    this.name = name 
+    this.id = ++passengerId
+    this.name = name
     store.passengers.push(this)
+  }
+
+}
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+    store.trips.push(this)
   }
   
 }
